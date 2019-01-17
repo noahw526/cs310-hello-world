1d4cfa34ca11b070a7985dbe51eefbb6aa4ca202
Noah Walker <nwalker11@stu.jsu.edu>
2019-01-17 15:21:41 -0600

    remove f3

--- Class1.java
+++ Class1.java
@@ -1,5 +1,5 @@
 /**
- *
+ *edited
  * @author Noah Walker
  */
 
@@ -8,9 +8,7 @@ public class Class1 {
     private void f1() {
         // v1.0
     }
-    private void f2() {
-        // v2.0
-    }
+    
     
 
 }
